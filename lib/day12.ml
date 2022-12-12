25261d6cf84c11fc1703d057e91b64962f50ffb0
Chris Carlon <cjc25@cjc25.com>
2022-12-12 01:51:32 -0500

    Day 12, neatened

--- lib/day12.ml
+++ lib/day12.ml
@@ -1,3 +1,4 @@
+open Containers
 open! Core
 open Types
 
@@ -9,101 +10,56 @@ module PrioXy = struct
   end
 
   include Comparable.Make (T)
+  module Heap = Heap.Make_from_compare (T)
 end
 
-let parta ls =
-  let start = ref (0, 0) in
-  let goal = ref (0, 0) in
+let solve ls is_start =
   let grid = Hashtbl.create (module Xy) in
-  List.iteri ls ~f:(fun y line ->
-      String.to_list line
-      |> List.iteri ~f:(fun x c ->
-             let c =
-               match c with
-               | 'S' ->
-                   start := (x, y);
-                   'a'
-               | 'E' ->
-                   goal := (x, y);
-                   'z'
-               | v -> v
-             in
-             let v = Char.to_int c in
-             Hashtbl.set grid ~key:(x, y) ~data:v));
-  let rec search seen es =
-    let res =
-      List.fold_until es ~init:(seen, [])
-        ~f:(fun (seen, news) (steps, pos) ->
-          if Xy.equal pos !goal then Stop (`Found steps)
-          else
-            let ht = Hashtbl.find_exn grid pos in
-            let es =
-              nabes pos
-              |> List.filter_map ~f:(fun nabe ->
-                     let cango =
-                       Hashtbl.find grid nabe
-                       |> Option.value_map ~default:false ~f:(fun nht ->
-                              nht <= ht + 1)
-                     in
-                     if Set.mem seen nabe || not cango then None
-                     else Some (steps + 1, nabe))
-            in
-            let nseen =
-              List.fold es ~init:seen ~f:(fun s (_, p) -> Set.add s p)
-            in
-            Continue (nseen, es @ news))
-        ~finish:(fun s -> `Cont s)
-    in
-    match res with
-    | `Found steps -> steps
-    | `Cont (seen, news) -> search seen news
+  let starts, goal =
+    List.foldi ls
+      ~init:([], (0, 0))
+      ~f:(fun y (starts, goal) line ->
+        String.to_list line
+        |> List.foldi ~init:(starts, goal) ~f:(fun x (starts, goal) c ->
+               let starts =
+                 is_start c
+                 |> Option.value_map ~default:starts ~f:(fun _ ->
+                        (x, y) :: starts)
+               in
+               let goal, c =
+                 match c with
+                 | 'S' -> (goal, 'a')
+                 | 'E' -> ((x, y), 'z')
+                 | v -> (goal, v)
+               in
+               Hashtbl.set grid ~key:(x, y) ~data:(Char.to_int c);
+               (starts, goal)))
   in
-  search Xy.Set.empty [ (0, !start) ] |> Printer.of_int
-
-let partb ls =
-  let starts = ref [] in
-  let goal = ref (0, 0) in
-  let grid = Hashtbl.create (module Xy) in
-  List.iteri ls ~f:(fun y line ->
-      String.to_list line
-      |> List.iteri ~f:(fun x c ->
-             let c =
-               match c with
-               | 'S' | 'a' ->
-                   starts := (x, y) :: !starts;
-                   'a'
-               | 'E' ->
-                   goal := (x, y);
-                   'z'
-               | v -> v
-             in
-             let v = Char.to_int c in
-             Hashtbl.set grid ~key:(x, y) ~data:v));
-  let found = ref None in
-  let q = List.map !starts ~f:(fun p -> (0, p)) |> PrioXy.Set.of_list |> ref in
-  let seen = ref Xy.Set.empty in
-  while Option.is_none !found do
-    let s, p = Set.min_elt_exn !q in
-    q := Set.remove_index !q 0;
-    if Xy.equal p !goal then found := Some s
-    else if Set.mem !seen p then ()
-    else
+  let seen = Hash_set.create (module Xy) in
+  let rec seek q =
+    let q, (s, p) = PrioXy.Heap.take_exn q in
+    if Xy.equal p goal then s
+    else if Hash_set.mem seen p then seek q
+    else (
+      Hash_set.add seen p;
       let ht = Hashtbl.find_exn grid p in
       let ns =
         nabes p
         |> List.filter_map ~f:(fun n ->
-               let cango =
-                 Hashtbl.find grid n
-                 |> Option.value_map ~default:false ~f:(fun nht ->
-                        nht <= ht + 1)
-               in
-               if cango then Some (s + 1, n) else None)
+               Hashtbl.find grid n
+               |> Option.bind ~f:(fun nht ->
+                      if nht <= ht + 1 then Some (s + 1, n) else None))
       in
-      q := PrioXy.Set.of_list ns |> Set.union !q
-  done;
-  Option.value_exn !found |> Printer.of_int
+      let q = PrioXy.Heap.add_list q ns in
+      seek q)
+  in
+  List.map starts ~f:(fun p -> (0, p))
+  |> PrioXy.Heap.of_list |> seek |> Printer.of_int
 
-let _sample =
+let parta ls = solve ls (function 'S' -> Some 'a' | _ -> None)
+let partb ls = solve ls (function 'S' | 'a' -> Some 'a' | _ -> None)
+
+let sample =
   {|Sabqponm
 abcryxxl
 accszExk
@@ -111,9 +67,9 @@ acctuvwj
 abdefghi|} |> String.split ~on:'\n'
 
 let%expect_test "a" =
-  parta _sample |> Printer.print;
+  parta sample |> Printer.print;
   [%expect {| 31 |}]
 
 let%expect_test "b" =
-  partb _sample |> Printer.print;
+  partb sample |> Printer.print;
   [%expect {| 29 |}]
