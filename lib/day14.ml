64d9b1a8141fc0b573838c0e34340a130b66e214
Chris Carlon <cjc25@cjc25.com>
2022-12-14 00:45:30 -0500

    Day 14, cleanup

--- lib/day14.ml
+++ lib/day14.ml
@@ -2,21 +2,22 @@ open! Containers
 open! Core
 open! Types
 
-let iter (xf, yf) (xt, yt) ~f =
+let ptiter ~f (xf, yf) (xt, yt) =
   let dx, dy =
     ( Int.clamp_exn ~min:(-1) ~max:1 (xt - xf),
       Int.clamp_exn ~min:(-1) ~max:1 (yt - yf) )
   in
   let rec loop (x, y) =
-    if Xy.equal (x, y) (xt, yt) then f (x, y)
-    else (
-      f (x, y);
-      loop (x + dx, y + dy))
+    match Xy.equal (x, y) (xt, yt) with
+    | true -> f (x, y)
+    | false ->
+        f (x, y);
+        loop (x + dx, y + dy)
   in
   loop (xf, yf)
 
-let parta ls =
-  let grid = Hash_set.create (module Xy) in
+let mkgrid_with_lowest ls =
+  let grid = Xy.Hash_set.create () in
   let lowest = ref 0 in
   List.iter ls ~f:(fun line ->
       let pts =
@@ -29,81 +30,61 @@ let parta ls =
       let rec set start = function
         | [] -> ()
         | next :: tl ->
-            iter start next ~f:(fun p ->
+            ptiter start next ~f:(fun p ->
                 Hash_set.add grid p;
                 lowest := Int.max !lowest (Xy.y p));
             set next tl
       in
       set (List.hd_exn pts) (List.tl_exn pts));
+  (grid, !lowest)
+
+let rec drop ~stop grid (x, y) =
+  let d = (x, y + 1) and dl = (x - 1, y + 1) and dr = (x + 1, y + 1) in
+  match
+    ( stop (x, y),
+      Hash_set.mem grid d,
+      Hash_set.mem grid dl,
+      Hash_set.mem grid dr )
+  with
+  | true, _, _, _ ->
+      Hash_set.add grid (x, y);
+      (x, y)
+  | _, false, _, _ -> drop ~stop grid d
+  | _, _, false, _ -> drop ~stop grid dl
+  | _, _, _, false -> drop ~stop grid dr
+  | _ ->
+      Hash_set.add grid (x, y);
+      (x, y)
+
+let parta ls =
+  let grid, lowest = mkgrid_with_lowest ls in
   let rec loop grain =
-    let rec drop (x, y) =
-      if y = !lowest then y
-      else
-        match
-          ( Hash_set.mem grid (x, y + 1),
-            Hash_set.mem grid (x - 1, y + 1),
-            Hash_set.mem grid (x + 1, y + 1) )
-        with
-        | false, _, _ -> drop (x, y + 1)
-        | _, false, _ -> drop (x - 1, y + 1)
-        | _, _, false -> drop (x + 1, y + 1)
-        | _ ->
-            Hash_set.add grid (x, y);
-            y
-    in
-    if drop (500, 0) = !lowest then grain else loop (grain + 1)
+    if drop ~stop:(fun (_, y) -> y = lowest) grid (500, 0) |> Xy.y = lowest then
+      grain
+    else loop (grain + 1)
   in
   loop 0 |> Printer.of_int
 
 let partb ls =
-  let grid = Hash_set.create (module Xy) in
-  let lowest = ref 0 in
-  List.iter ls ~f:(fun line ->
-      let pts =
-        Input.split_on_string line ~sep:" -> "
-        |> List.map ~f:(String.split ~on:',')
-        |> List.map ~f:(function
-             | [ xs; ys ] -> (Int.of_string xs, Int.of_string ys)
-             | _ -> failwith "bad line")
-      in
-      let rec set start = function
-        | [] -> ()
-        | next :: tl ->
-            iter start next ~f:(fun p ->
-                Hash_set.add grid p;
-                lowest := Int.max !lowest (Xy.y p));
-            set next tl
-      in
-      set (List.hd_exn pts) (List.tl_exn pts));
+  let grid, lowest = mkgrid_with_lowest ls in
   let rec loop grain =
-    let rec drop (x, y) =
-      if y = !lowest + 1 then (
-        Hash_set.add grid (x, y);
-        (x, y))
-      else
-        match
-          ( Hash_set.mem grid (x, y + 1),
-            Hash_set.mem grid (x - 1, y + 1),
-            Hash_set.mem grid (x + 1, y + 1) )
-        with
-        | false, _, _ -> drop (x, y + 1)
-        | _, false, _ -> drop (x - 1, y + 1)
-        | _, _, false -> drop (x + 1, y + 1)
-        | _ ->
-            Hash_set.add grid (x, y);
-            (x, y)
-    in
-    if Xy.equal (drop (500, 0)) (500, 0) then grain else loop (grain + 1)
+    if
+      drop ~stop:(fun (_, y) -> y = lowest + 1) grid (500, 0)
+      |> Xy.equal (500, 0)
+    then grain
+    else loop (grain + 1)
   in
   loop 1 |> Printer.of_int
 
-let _sample = {|498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9|} |> String.split ~on:'\n'
+let _sample =
+  {|498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9|}
+  |> String.split ~on:'\n'
 
 let%expect_test "a" =
-   parta _sample |> Printer.print;
-   [%expect {| 24 |}]
+  parta _sample |> Printer.print;
+  [%expect {| 24 |}]
 
 let%expect_test "b" =
-   partb _sample |> Printer.print;
-   [%expect {| 93 |}]
+  partb _sample |> Printer.print;
+  [%expect {| 93 |}]
