41683793ed0fc2ec2f9b7a1568d1c2765c663075
Chris Carlon <cjc25@cjc25.com>
2022-12-16 18:23:04 -0500

    Day 16, cleaned up to use DP in both parts.

--- lib/day16.ml
+++ lib/day16.ml
@@ -2,89 +2,6 @@ open! Containers
 open! Core
 open! Types
 
-module DState = struct
-  type t = {
-    cflow : int;
-    flow : int;
-    loc : string;
-    on : String.Set.t;
-    minute : int;
-  }
-end
-
-module Loc = struct
-  type t = { lflow : int; ts : string list }
-end
-
-module PosSteps = struct
-  module T = struct
-    type t = { l : string; s : int } [@@deriving compare, hash, sexp]
-  end
-
-  include T
-  include Hashable.Make (T)
-end
-
-let parta ls =
-  let g = String.Table.create () in
-  List.iter ls ~f:(fun s ->
-      let l, r =
-        match Input.split_on_string s ~sep:" valve" with
-        | [ l; r ] ->
-            ( l,
-              r
-              |> String.chop_prefix_if_exists ~prefix:"s"
-              |> String.chop_prefix_if_exists ~prefix:" " )
-        | _ -> failwithf "bad line %s" s ()
-      in
-      let ts = Input.split_on_string r ~sep:", " in
-      let l, lflow =
-        Scanf.sscanf l "Valve %s has flow rate=%d; %s" (fun n r _ -> (n, r))
-      in
-      Hashtbl.set g ~key:l ~data:{ Loc.lflow; ts });
-  let bestat = PosSteps.Table.create () in
-  let rec seek max = function
-    | [] -> max
-    | { DState.cflow; flow; loc; on; minute } :: tl -> (
-        let cflow = cflow + flow in
-        let max = Int.max max cflow in
-        if minute = 30 then seek max tl
-        else
-          match
-            Hashtbl.find bestat { l = loc; s = minute }
-            |> Option.value_map ~default:true ~f:(fun bestflow ->
-                   flow >= bestflow)
-          with
-          | false -> seek max tl
-          | true ->
-              Hashtbl.set bestat ~key:{ l = loc; s = minute } ~data:flow;
-              let { Loc.lflow; ts } = Hashtbl.find_exn g loc in
-              let on_here =
-                match Set.mem on loc || lflow = 0 with
-                | true ->
-                    [ { DState.cflow; flow; loc; on; minute = minute + 1 } ]
-                | false ->
-                    [
-                      {
-                        DState.cflow;
-                        flow = flow + lflow;
-                        loc;
-                        on = Set.add on loc;
-                        minute = minute + 1;
-                      };
-                    ]
-              in
-              let moves =
-                List.map ts ~f:(fun loc ->
-                    { DState.cflow; flow; loc; on; minute = minute + 1 })
-              in
-              let news = on_here @ moves @ tl in
-              seek max news)
-  in
-  seek 0
-    [ { cflow = 0; flow = 0; loc = "AA"; on = String.Set.empty; minute = 1 } ]
-  |> Printer.of_int
-
 module LToFro = struct
   module T = struct
     type t = string * string [@@deriving compare, hash, sexp]
@@ -103,7 +20,7 @@ module DPK = struct
   include Hashable.Make (T)
 end
 
-let partb ls =
+let build_dp ls =
   let flows = String.Table.create () in
   let dists = LToFro.Table.create () in
   let ns =
@@ -146,11 +63,12 @@ let partb ls =
     Hashtbl.fold flows ~init:String.Set.empty ~f:(fun ~key:l ~data:f s ->
         if f > 0 then Set.add s l else s)
   in
-  (* DP: each minute has a table of max seen (loc, on) -> cumulative flow. Work forward by minutes. *)
-  let total_mins = 27 in
-  let dp = Array.init total_mins ~f:(fun _ -> DPK.Table.create ()) in
+  (* DP: the end of each minute has a table of max seen (loc, on) -> cumulative
+     flow. Work forward by minutes. *)
+  let total_mins = 30 in
+  let dp = Array.init (total_mins + 1) ~f:(fun _ -> DPK.Table.create ()) in
   Hashtbl.set dp.(0) ~key:("AA", []) ~data:0;
-  for i = 0 to total_mins - 2 do
+  for i = 0 to total_mins - 1 do
     Hashtbl.iteri dp.(i) ~f:(fun ~key:(l, on) ~data:cf ->
         let fr =
           List.sum (module Int) on ~f:(fun v -> Hashtbl.find_exn flows v)
@@ -167,7 +85,7 @@ let partb ls =
         Set.diff valid_valves onset
         |> Set.iter ~f:(fun v ->
                let t = Hashtbl.find_exn dists (l, v) + 1 in
-               if t + i >= total_mins then ()
+               if t + i > total_mins then ()
                else
                  let new_on = Set.add onset v |> Set.to_list in
                  let newcf = cf + (fr * t) in
@@ -178,10 +96,19 @@ let partb ls =
                      (Option.value_map ~default:newcf ~f:(fun ef ->
                           Int.max ef newcf))))
   done;
-  (* Finally, for all pairs of results at minute 26 (index 25) with disjoint ons, take the max. *)
-  (* sort them, get the best f1, f2 pair where f1 >= f2. *)
+  dp
+
+let parta ls =
+  let dp = build_dp ls in
+  Hashtbl.fold dp.(30) ~init:0 ~f:(fun ~key:_ ~data:f mf -> Int.max f mf)
+  |> Printer.of_int
+
+let partb ls =
+  let dp = build_dp ls in
+  (* For all pairs of results after minute 26 with disjoint ons, take the max.
+     We do that by sorting them, get the best f1, f2 pair where f1 >= f2. *)
   let paths_desc =
-    Hashtbl.to_alist dp.(total_mins - 1)
+    Hashtbl.to_alist dp.(26)
     |> List.map ~f:(fun ((l, on), f) -> (String.Set.of_list on, f))
     |> List.sort ~compare:(fun (_, f1) (_, f2) -> Int.compare f2 f1)
   in
